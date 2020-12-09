6e20e5bf88a40fa1074ab6d2b6a26c1d2c12496f
chichi <jang.chihun@gmail.com>
2020-12-09 16:47:32 +0900

    Mdf Change Company address

--- src/views/LandingPage/Sections/WorkSection.js
+++ src/views/LandingPage/Sections/WorkSection.js
@@ -10,9 +10,9 @@ import GridItem from "components/Grid/GridItem.js";
 // import CustomInput from "components/CustomInput/CustomInput.js";
 
 import Button from "components/CustomButtons/Button.js";
-import EmailIcon from '@material-ui/icons/Email';
-import CallIcon from '@material-ui/icons/Call';
-import PetsIcon from '@material-ui/icons/Pets';
+import EmailIcon from "@material-ui/icons/Email";
+import CallIcon from "@material-ui/icons/Call";
+import PetsIcon from "@material-ui/icons/Pets";
 import styles from "assets/jss/material-kit-react/views/landingPageSections/workStyle.js";
 
 const useStyles = makeStyles(styles);
@@ -25,26 +25,46 @@ export default function WorkSection() {
         <GridItem cs={12} sm={12} md={8}>
           <h2 className={classes.title}>Contact us</h2>
           <h4 className={classes.description}>
-            항상 열려있습니다.. 여러분...<br />
+            항상 열려있습니다.. 여러분...
+            <br />
             언제든 콜미
           </h4>
           <form>
             <GridContainer>
               <GridItem xs={12} sm={12} md={6}>
-                <div className="info-title"><CallIcon />전화번호</div>
+                <div className="info-title">
+                  <CallIcon />
+                  전화번호
+                </div>
                 <div className="info-content">010-6684-0078</div>
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
-                <div className="info-title"><EmailIcon className="info-icon" />이메일</div>
+                <div className="info-title">
+                  <EmailIcon className="info-icon" />
+                  이메일
+                </div>
                 <div className="info-content">30bpm.official@gmail.com</div>
-                <Button 
-                  color="primary"
-                  href="mailto:30bpm.official@gmail.com">이메일 전송</Button>
+                <Button color="primary" href="mailto:30bpm.official@gmail.com">
+                  이메일 전송
+                </Button>
               </GridItem>
               <GridItem xs={12} sm={12} md={6}>
-                <div className="info-title"><PetsIcon />우리집</div>
+                <div className="info-title">
+                  <PetsIcon />
+                  우리집
+                </div>
               </GridItem>
-              <iframe id="our-nest" src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3261.334756489299!2d129.12824275141008!3d35.173208180219255!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356892c1b7cef823%3A0x353323e15563c37d!2z7IS87YWA6riw7Iig7LC97JeF7YOA7Jq0KENFTlRBUCk!5e0!3m2!1sko!2skr!4v1606872381208!5m2!1sko!2skr" width="600" height="450" frameborder="0" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+
+              <iframe
+                id="our-nest"
+                src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3259.336128530352!2d129.0829517152463!3d35.223001780303704!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x356893911416e1d1%3A0x21472e5d3787995f!2z67aA7IKw6rSR7Jet7IucIOq4iOygleq1rCDsnqXsoIQz64-ZIOyYqOyynOyepeuhnCAxMzkgQTQwMe2YuA!5e0!3m2!1sko!2skr!4v1607499727597!5m2!1sko!2skr"
+                width="600"
+                height="450"
+                frameborder="0"
+                allowfullscreen=""
+                aria-hidden="false"
+                tabindex="0"
+              ></iframe>
             </GridContainer>
           </form>
         </GridItem>
